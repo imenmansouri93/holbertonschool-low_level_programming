26f6d382533132fc34863911bec07a9dcaa5733a
imenmansouri93 <imennmansourii@gmail.com>
2022-06-06 14:03:25 -0700

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,11 +10,11 @@ char charType;
  *
  *Return: 0
  **/
+	printf("Size of char: %zu byte\n", sizeof(charType));
 	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
 	printf("Size of long int: %zu bytes\n", sizeof(longIntType));
 	printf("Size of long long int: %zu bytes\n", sizeof(longLongInt));
-	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
 
 	return (0);
 }
