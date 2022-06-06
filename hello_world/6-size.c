07f5f5bc5e936b443aa2d71c8530964df1dd2e46
imenmansouri93 <imennmansourii@gmail.com>
2022-06-06 13:59:11 -0700

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
-int main() {
+int main(void) {
 int intType;
 float floatType;
-double doubleType;
+long int longIntType;
+long long int longLongInt;
 char charType;
 /*
  *main - prints the size of various types on the computer it is compiled and run on.
@@ -11,7 +12,8 @@ char charType;
  **/
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of long int: %zu bytes\n", sizeof(longIntType));
+	printf("Size of long long int: %zu bytes\n", sizeof(longLongInt));
 	printf("Size of char: %zu byte\n", sizeof(charType));
 
 	return (0);
