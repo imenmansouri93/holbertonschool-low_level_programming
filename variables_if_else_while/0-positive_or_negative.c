532248d738fb5dc71b02ed7ddf836e3878077c58
mansouri imen <mansouriimen05@gmail.com>
2022-06-07 20:55:24 +0200

    task0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,15 @@
- #include <stdio.h>
- #include <stdlib.h>
- #include <time.h>
- 
- /**
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
- int main(void)
- {
+int main(void)
+{
     int n;
 
     srand(time(0));
@@ -30,4 +30,4 @@
     }
 
     return (0);
- }
+}
