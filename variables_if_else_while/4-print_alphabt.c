1ac983586ef514bccdf03d4c8596f30ab08408c2
imenmansouri93 <imennmansourii@gmail.com>
2022-06-08 23:16:20 +0100

    print alphabet exept e and q

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-    char ch;
-    for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        if (ch != 'q' && ch != 'e')
-        {
-            putchar(ch);
-        }
-    }
-    putchar('\n');
-    return (0);
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
 }
