ade477c58c6b3af0a184eabe1f61dbf1dba87ef8
imenmansouri93 <imennmansourii@gmail.com>
2022-06-09 16:01:20 +0100

    print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+  * main - to print _putchar
+  *
+  * Return: Always 0 (Success)
+  **/
+int main()
+{
+char ch = '_putchar';
+putchar(ch);
+putchar('\n');
+return (0);
+}
+
