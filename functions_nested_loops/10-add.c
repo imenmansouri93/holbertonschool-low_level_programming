da7b195efc50473db149cf5c190a01a7f6e492e2
imenmansouri93 <imennmansourii@gmail.com>
2022-06-09 22:18:04 +0100

    adds two integers and returns the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ *@A:int
+ *@B:int
+ * Return: somme
+ */
+
+int add(int A, int B)
+{
+int sum;
+sum = A + B;
+return (sum);
+}
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @x: check number
+ * Return: last digit
+ */
+void jack_bauer(void)
