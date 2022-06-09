ce88fdc136be06f47a373996cff676bec0e42663
imenmansouri93 <imennmansourii@gmail.com>
2022-06-09 19:55:44 +0100

    checks for lowercase characte

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - checks for lowercase characte
+ * @c: lowercase
+ * Return: 1 if c is lowercase Return:  0 otherwise
+ */
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
