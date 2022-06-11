8b8da65551db011f34fc59addf77a9603bfb6fdd
imenmansouri93 <imennmansourii@gmail.com>
2022-06-11 01:16:45 +0100

    void print_times_table

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_times_table - function that prints the n times table
+ *@n:natural number
+ *
+ */
+void print_times_table(int n)
+{
+int i;
+int j;
+for (i = 0; i < n + 1; i++)
+{
+for (j = 0; j < n + 1; j++)
+{
+if (j != n)
+{
+printf("%d, ",  i * j);
+}
+else
+{
+printf("%d", i * j);
+}
+}
+printf("\n");
+}
+}
