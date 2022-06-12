482645a7fa4663c8c6926f45b661162830d4649d
imenmansouri93 <imennmansourii@gmail.com>
2022-06-12 23:45:23 +0100

    prints a square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  print_square - function that draws a straight line in the terminal.
+ *  print_square - function that draws a square.
  * @size: number > 0
  * Return: Always 0 (Success)
  */
@@ -10,10 +10,10 @@ void print_square(int size)
 int i, j;
 if (size > 0)
 {
-for (i = 0; i <= size; i++)
+for (i = 0; i < size; i++)
 {
-for (j = 0; j <= size; j++)
-_putchar('#');
+for (j = 0; j < size; j++)
+	_putchar('#');
 _putchar('\n');
 }
 }
