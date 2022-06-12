6ef6c092147d894c0a027f7983824ffb26ac7cdc
imenmansouri93 <imennmansourii@gmail.com>
2022-06-12 12:40:23 +0100

    headerfile

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/* File: main.h
+ *
+ *
+ *header file containing the functions_nested
+ */
+int _putchar(char c);
+int _isupper(int c);
