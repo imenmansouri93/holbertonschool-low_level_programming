588962de0855c42d8497bdbcbd8dfb19d8dcb650
imenmansouri93 <imennmansourii@gmail.com>
2022-06-13 20:19:59 +0100

    headerfile

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+
 /* File: main.h
  *
  *
@@ -10,3 +11,4 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
