cdae5d64545be95ad03c0ba34d59f285321f1273
imenmansouri93 <imennmansourii@gmail.com>
2022-06-16 16:56:15 +0100

    function that capitalizes all words of a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,30 +6,27 @@
  */
 char *cap_string(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i++)
+int i = 0;
+while (str[i])
 {
-if (i == 0)
-{
-if ((str[i] >= 'a' && str[i] <= 'z'))
-str[i] = str[i] - 32;
-continue;
+while (!(str[i] >= 'a' && str[i] <= 'z'))
+i++;
+if (str[i - 1] == ' ' ||
+str[i - 1] == ',' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == '-' ||
+str[i - 1] == '}' ||
+str[i - 1] == '{' ||
+str[i - 1] == '(' ||
+str[i - 1] == ')' ||
+str[i - 1] == ';' ||
+str[i - 1] == '"' ||
+i == 0)
+str[i] -= 32;
 i++;
 }
-if (str[i] == ' ')
-{
-++i;
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 32;
-continue;
-}
-}
-else
-{
-if (str[i] >= 'A' && str[i] <= 'Z')
-str[i] = str[i] + 32;
-}
-}
-	return (0);
+return (str);
 }
