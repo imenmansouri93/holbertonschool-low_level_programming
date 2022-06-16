35366e657bce23d39cbe72d8e5dc267f97bca77f
imenmansouri93 <imennmansourii@gmail.com>
2022-06-16 20:43:59 +0100

    function that copies memory area.

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_memcpy -  function that copies memory area.
+ * @dest: destination
+ * @src: source
+ * @n: integer
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *pdest = dest;
+char  *psrc = src;
+while (n--)
+{
+{
+*pdest++ = *psrc++;
+}
+}
+return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -29,3 +29,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *str);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
