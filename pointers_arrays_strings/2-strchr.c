d331cbca5f34ec361e7c59217eae5049e695f7aa
imenmansouri93 <imennmansourii@gmail.com>
2022-06-16 21:12:40 +0100

    function that locates a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<string.h>
+/**
+* _strchr - function that locates a character in a string.
+* @s: char
+* @c: char
+* Return: s
+*/
+char *_strchr(char *s, char c)
+{
+while (*s != (char) c)
+if (!*s++)
+{
+return (NULL);
+}
+return ((char *)s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -30,3 +30,4 @@ char *string_toupper(char *s);
 char *cap_string(char *str);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
