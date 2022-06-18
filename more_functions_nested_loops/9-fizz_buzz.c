9a29ac95417232792ed1cb3b7fb66f3e5e1e015d
imenmansouri93 <imennmansourii@gmail.com>
2022-06-18 18:23:56 +0100

    fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -18,13 +18,12 @@ else if (i % 3 == 0)
 {
 printf(" Fuzz ");
 }
-else if (i % 5 == 0 )
+else if (i % 5 == 0)
 {
 printf(" Buzz ");
-}else
-{
-printf("%d ", i);
 }
+else
+printf("%d ", i);
 }
 printf("\n");
 return (0);
