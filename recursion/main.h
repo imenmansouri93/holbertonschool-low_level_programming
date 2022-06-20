b786df7eb4d4f68ea89df9c9e7e4527c3f6549ce
imenmansouri93 <imennmansourii@gmail.com>
2022-06-20 21:52:32 +0100

    headerfile

---
+++ recursion/main.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stddef.h>
+
+/* File: main.h
+ *
+ *
+ * header file containing the functions_nested
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
