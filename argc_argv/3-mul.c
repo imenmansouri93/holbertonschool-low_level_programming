6a31dbe2e5644e699784f2e14fe0c7c919e65c5f
imenmansouri93 <imennmansourii@gmail.com>
2022-06-23 21:11:18 +0100

    program that multiplies two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - function print all arguments
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int x1, x2;
+if (argc == 3)
+{
+x1 = atoi(argv[1]);
+x2 = atoi(argv[2]);
+printf(" %d\n", x1 *x2);
+}
+else
+{
+printf("error\n");
+return (1);
+}
+return (0);
+}
