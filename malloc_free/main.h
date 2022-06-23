a25625837fd2072d30fc17a1d4ec03ff44c1c37b
imenmansouri93 <imennmansourii@gmail.com>
2022-06-24 00:39:48 +0100

    headerfile

---
+++ malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stddef.h>
+
+/* File: main.h
+ *
+ *
+ * header file containing the functions_nested
+ */
+char *create_array(unsigned int size, char c);
+
+#endif
