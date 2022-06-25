da1ee45d136a9a4fd0437aa4c333fe0ff2808a78
imenmansouri93 <imennmansourii@gmail.com>
2022-06-25 23:48:29 +0100

    function that returns a pointer to a 2 dim arr of int

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,22 +2,29 @@
 /**
  * **alloc_grid-function that returns a pointer to a 2 dim arr of int.
  * @width: int
- * @height: int;
- * Return: array
+ * @height: int
+ * Return: pointer to 2- D array
  */
 int **alloc_grid(int width, int height)
 {
 int i;
 int **arr;
-arr = (int **)malloc(sizeof(int) * height);
 if (width <= 0 || height <= 0)
 {
 return (NULL);
 }
-else
+arr = (int **)malloc(height * sizeof(int));
+if (arr == NULL)
+{
+return (NULL);
+}
 for (i = 0; i < height; i++)
 {
-arr[i] = (int *)malloc(sizeof(int) * width);
+arr[i] = malloc(width * sizeof(int));
+if (arr[i] == NULL)
+{
+return (NULL);
+}
 }
 return (arr);
 }
