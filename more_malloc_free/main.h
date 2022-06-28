17a15836ab7618e818b271f4ccf445c53cc5bd53
imenmansouri93 <imennmansourii@gmail.com>
2022-06-28 13:32:21 +0100

    headerffile

---
+++ more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stddef.h>
+/**
+ * File: main.h
+ * 
+ * header file containing the functions_nested
+ */
+void *malloc_checked(unsigned int b);
+#endif
