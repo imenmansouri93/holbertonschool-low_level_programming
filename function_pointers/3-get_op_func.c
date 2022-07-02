39eb1cfd8ea0998af414e9d7be9ca58b10b40bd8
imenmansouri93 <imennmansourii@gmail.com>
2022-07-02 23:36:08 +0100

    get function

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,6 +1,21 @@
+#include "3-calc.h"
+#include <string.h>
 int (*get_op_func(char *s))(int, int)
 {
-    int (*op[4](int, int));
-    
-
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}};
+int i;
+for (i = 0; ops[i] < 5; i++)
+{
+if (strcmp(s, ops[i]) == 0)
+{
+return (ops[i].f)
+}
+}
+return(NULL);
 }
