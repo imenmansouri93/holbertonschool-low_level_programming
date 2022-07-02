868467668bc20b89f305088bae7865680fa3f3ed
imenmansouri93 <imennmansourii@gmail.com>
2022-07-02 18:42:32 +0100

    file should contain the 5 following functions

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+int op_add(int a, int b)
+{
+return (a  + b);
+}
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+int op_mul(int a, int b)
+{
+retuurn (a * b);
+}
+int op_div(int a, int b)
+{
+if (b != 0)
+{
+    return (a / b);
+}
+return ("error");
+}
+int op_mod(int a, int b)
+{
+if (b != 0)
+{
+  return (a % b);
+}
+return ("error");
+}
