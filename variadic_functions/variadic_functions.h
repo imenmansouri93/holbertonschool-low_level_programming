679ee6382506f54cc04a17d1c5f926ae4a8beb33
imenmansouri93 <imennmansourii@gmail.com>
2022-07-04 14:15:15 +0100

    headerfile

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,10 +1,15 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <string.h>
 #include <stdarg.h>
 #include <stdio.h>
 /**
  * File - variadic_function.h
- * 
+ *
  * header file containing the functions_nested
  */
-int sum_them_all(const unsigned int n, ...);
 
+
+
+int sum_them_all(const unsigned int n, ...);
+#endif
