fa6964dca4f3635153e3e51e301198cdfa6555b8
imenmansouri93 <imennmansourii@gmail.com>
2022-07-04 16:27:37 +0100

    prints numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -4,22 +4,22 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * 
- * 
- * 
+ * print_numbers - function that prints numbers
+ * @separator: char
+ * @n: int
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list al;
 unsigned int i;
 va_start(al, n);
-for (i  = 0; i<n; i++)
+for (i  = 0; i < n; i++)
 {
 printf("%d", va_arg(al, int));
-if (i != (n-1) && separator != NULL)
+if (i != (n - 1) && separator != NULL)
 printf("%s", separator);
 }
- printf ("\n");
+printf("\n");
 va_end(al);
 }
 #endif
