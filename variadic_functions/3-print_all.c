2d99c75e9af22ccb64da24a8634ee80839e9f0ae
imenmansouri93 <imennmansourii@gmail.com>
2022-07-05 20:09:27 +0100

    print-all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,7 +13,11 @@
  *
  *
  */
-
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char *const format, ...);
 void print_char(va_list arg)
 {
     char letter;
