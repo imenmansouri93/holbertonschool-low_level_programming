0b291b95adf8aa6c7ef2770d157bf392826c8d62
imenmansouri93 <imennmansourii@gmail.com>
2022-07-05 20:07:15 +0100

    print-all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,84 +4,87 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * 
- * 
- * 
+ *
+ *
+ *
  */
 
 /**
- * 
- * 
+ *
+ *
  */
-void print_all(const char * const format, ...)
-{
-va_list args;
-format_t typeformat[] = {
-{"c", print_char},
-{"i", print_int},
-{"d", print_float},
-{"s", print_string},
-};
-unsigned  int i = 0;
-unsigned int j = 0;
-char *separator = "";
-va_start(args, format);
-while (format != NULL && format[i])
-{
-j = 0;
-while (j < 4)
-{
-if (format[i] == *typeformat[j].type)
-{
-printf("%s", separator);
-typeformat[j].f(args);
-separator = ", ";
-}
-j++;
-}
-i++;
-}
-va_end(args);
-printf("\n");
-}
 
 void print_char(va_list arg)
 {
-va_arg(arg, int);
-printf("%c", va_arg(arg, int));
+    char letter;
+    letter = va_arg(arg, int);
+    printf("%c", letter);
 }
 /**
- * 
- * 
+ *
+ *
  */
 void print_int(va_list arg)
 {
-    va_arg(arg, int);
-    printf("%i",va_arg(arg, int));
+    int num;
+    num = va_arg(arg, int);
+    printf("%i", num);
 }
 /**
- * 
- * 
+ *
+ *
  */
 void print_float(va_list arg)
 {
-    va_arg(arg, double);
-    printf("%f", va_arg(arg, double));
+    float num;
+    num = va_arg(arg, double);
+    printf("%f", num);
 }
 /**
- * 
- * 
+ *
+ *
  */
 void print_string(va_list arg)
 {
-char *str;
-str = va_arg(arg, char *);
-if (str != NULL)
-printf("%s", str);
+    char *str;
+    str = va_arg(arg, char *);
+    if (str == NULL)
+    {
+        printf("(nil)");
+    }
+    printf("%s", str);
 }
 /**
- * 
- * 
+ *
+ *
  */
-
+void print_all(const char *const format, ...)
+{
+    va_list args;
+    unsigned int i = 0;
+    unsigned int j = 0;
+    char *separator = "";
+    format_t typeformat[] = {
+        {"c", print_char},
+        {"i", print_int},
+        {"d", print_float},
+        {"s", print_string},
+    };
+    va_start(args, format);
+    while (format && (*(format +i )))
+    {
+        j = 0;
+        while (j < 4 && (*(format + i) != *(typeformat[j].type)))
+            j++;
+        if (j < 4)
+        {
+            printf("%s", separator);
+            typeformat[j].f(args);
+            separator = ", ";
+        }
+        i++;
+    }
+    printf("\n");
+    va_end(args);
+}
 #endif
