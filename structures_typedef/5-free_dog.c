23aba6306873ecf3e8df7ac112a64658d5e70db9
imenmansouri93 <imennmansourii@gmail.com>
2022-07-07 04:22:48 +0100

    freedog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,15 +2,18 @@
 #define NEW_DOG_C
 #include<stdlib.h>
 #include "dog.h"
+/**
+ * free_dog - free memory
+ * @d: ...
+ * Return: NNothing
+ */
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
+if (d != NULL)
+{
 free(d->owner);
 free(d->name);
 free(d);
-    }
-    
-   
+}
 }
 #endif
