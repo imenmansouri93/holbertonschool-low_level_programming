b10950bc829b6c48689915917ddceb6d771e1912
imenmansouri93 <imennmansourii@gmail.com>
2022-07-07 04:11:48 +0100

    newdog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,13 +3,13 @@
 #include<stdlib.h>
 #include "dog.h"
 int _strlen(char *s);
-char *_strcpy(char *dest, char*src);
+char *_strcpy(char *dest, char *src);
 /**
  * new_dog - function that creates a new dog
  * @name: variable pointing to name of dog
  * @age: age of dog
  * @owner: variable pointing to dog owner
- * Return: variable pointing to dog-t struct 
+ * Return: variable pointing to dog-t struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -18,9 +18,9 @@ int name1 = 0, owner1 = 0;
 
 if (name != NULL && owner != NULL)
 {
-name1 = _strlen(name) +1;
+name1 = _strlen(name) + 1;
 owner1 = _strlen(owner) + 1;
-my_dog = malloc (sizeof(dog_t));
+my_dog = malloc(sizeof(dog_t));
 if (my_dog == NULL)
 return (NULL);
 my_dog->name = malloc(sizeof(char) * name1);
@@ -34,18 +34,18 @@ if (my_dog->owner ==  NULL)
 {
 free(my_dog->name);
 free(my_dog);
-return(NULL);
+return (NULL);
 }
 my_dog->name = _strcpy(my_dog->name, name);
-my_dog->owner = _strcpy(my_dog->owner,owner);
+my_dog->owner = _strcpy(my_dog->owner, owner);
 my_dog->age = age;
 }
-return(my_dog);
+return (my_dog);
 }
 /**
- * 
- * 
- * 
+ * _strlen - function that return length of string
+ * @s: string
+ * Return: length of string
  */
 int _strlen(char *s)
 {
@@ -56,16 +56,22 @@ while (s[i] != '\0')
 l++;
 i++;
 }
-return(l);
+return (l);
 }
-char *_strcpy(char *dest, char*src)
+/**
+ * *_strcpy - function that copy string
+ * @dest: string
+ * @src: string
+ * Return: ...
+ */
+char *_strcpy(char *dest, char *src)
 {
 int i;
-for ( i = 0; src[i] != '\0' ; i++)
+for (i = 0; src[i] != '\0' ; i++)
 {
-  dest[i] = src[i];
+dest[i] = src[i];
 }
 dest[i++] = '\0';
-return(dest);
+return (dest);
 }
 #endif
