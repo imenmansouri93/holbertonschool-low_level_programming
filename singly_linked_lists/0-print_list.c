1688d0021e4e0ad353de8224bae05a95cbf86931
imenmansouri93 <imennmansourii@gmail.com>
2022-07-13 15:35:35 +0100

    print-list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,26 +1,23 @@
 #include "lists.h"
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to the start of the list
+ * Return: number of nodes in the list
  */
 
 
 size_t print_list(const list_t *h)
 {
-    const list_t *tmp;
-    unsigned int i;
-    tmp = h;
-    for (i = 0; tmp != NULL; i++)
-    {
-    if  (tmp -> str == NULL)
-    printf("[0] (nil)\n");
-    else
-    printf("[%u] %s\n", tmp -> len, tmp -> str);
-    tmp = tmp -> next;
-    }
-    return(i);
+const list_t *tmp;
+unsigned int i;
+tmp = h;
+for (i = 0; tmp != NULL; i++)
+{
+if  (tmp->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", tmp->len, tmp->str);
+tmp = tmp->next;
+}
+return (i);
 }
