d356f851466654a415e4546544596ab0adaf4dad
imenmansouri93 <imennmansourii@gmail.com>
2022-07-14 00:42:07 +0100

    freelist

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list - free list
+ * @head: pointer to the start of the list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+list_t *tmp;
+while (head != NULL)
+{
+tmp = head;
+head = head->next;
+free(tmp);
+}
+}
