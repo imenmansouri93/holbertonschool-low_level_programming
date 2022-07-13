ea1c65c26cad5290a48a72519a1f68406412b8e8
imenmansouri93 <imennmansourii@gmail.com>
2022-07-13 15:23:28 +0100

    headerfile

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @brief 
+ * 
+ * @param h 
+ * @return size_t 
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+#endif
