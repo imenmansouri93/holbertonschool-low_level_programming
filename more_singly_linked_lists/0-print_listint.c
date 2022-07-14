306659c554bb0f4c8dfaf5088b75afa1734b05ca
imenmansouri93 <imennmansourii@gmail.com>
2022-07-14 15:29:04 +0100

    pprintlistint

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * @brief 
+ * 
+ */
+size_t print_listint(const listint_t *h)
+{
+ const listint_t *tmp;
+unsigned int i;
+tmp = h;
+for (i = 0; tmp != NULL; i++)
+{
+if (tmp->n == 0)
+printf("0\n");
+else
+printf(" %i\n", tmp->n);
+tmp = tmp->next;
+}
+return (i);
+}
