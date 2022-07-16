bf373833a6f77398f01a2a7e67f377e283dd86a4
imenmansouri93 <imennmansourii@gmail.com>
2022-07-16 18:57:18 +0100

    insertnode

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,28 +1,35 @@
 #include "lists.h"
+/**
+ * insert_nodeint_at_index - that inserts a new node at a given position
+ * @head: pointer to the start of the linkedlist
+ * @idx: index
+ * @n: data
+ * Return: listint_t
+ */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)   
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t* new_node, *copy = *head;
+listint_t  *new_node, *copy = *head;
 unsigned int node;
-new_node = (listint_t*)malloc(sizeof(listint_t));
+new_node = (listint_t *)malloc(sizeof(listint_t));
 if (new_node == NULL)
 {
-return(NULL);
+return (NULL);
 }
-new_node ->n = n;
-if(idx == 0)
+new_node->n = n;
+if (idx == 0)
 {
 new_node->next = copy;
 *head = new_node;
-return(new_node);
+return (new_node);
 }
 for (node = 0; node < (idx - 1); node++)
 {
 if (copy == NULL || copy->next == NULL)
-return(NULL);
+return (NULL);
 copy = copy->next;
 }
 new_node->next = copy->next;
 copy->next = new_node;
-return(new_node);
+return (new_node);
 }
