1b001498d6a53d1332d7c326033fdbba5a94877a
imenmansouri93 <imennmansourii@gmail.com>
2022-07-16 18:05:00 +0100

    deletenode

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 /**
- * @brief 
- * 
- * 
+ * delete_nodeint_at_index - that deletes the node at index of alinked list.
+ * @head: pointer to the start of the list
+ * @index: index of the node that should be deleted
+ * Return: 1
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 unsigned int count = 1;
 listint_t *new = *head, *tmp;
-if (!head || !*head)
-return(-1);
+if (!head)
+return (-1);
 if (index == 0)
 {
 *head = new->next;
 free(new);
-return(1);
+return (1);
 }
 tmp = *head;
 while (tmp)
@@ -24,10 +25,10 @@ if (count == index)
 new = tmp->next;
 tmp->next = new->next;
 free(new);
-return(1);
+return (1);
 }
 tmp = tmp->next;
 count++;
 }
-return(-1);
+return (-1);
 }
