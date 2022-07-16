1bb86b44eb937e99c24f83a01eabd73216312cfc
imenmansouri93 <imennmansourii@gmail.com>
2022-07-16 18:06:42 +0100

    deletenode

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 unsigned int count = 1;
 listint_t *new = *head, *tmp;
-if (!head)
+if (!*head)
 return (-1);
 if (index == 0)
 {
