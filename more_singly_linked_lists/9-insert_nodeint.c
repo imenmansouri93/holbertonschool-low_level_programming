0ea90e1922e2654e688375e85c8fb06504b3d0f7
imenmansouri93 <imennmansourii@gmail.com>
2022-07-16 18:14:20 +0100

    insertnode

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)   
+{
+listint_t* new_node, *copy = *head;
+unsigned int node;
+new_node = (listint_t*)malloc(sizeof(listint_t));
+if (new_node == NULL)
+{
+return(NULL);
+new_node ->n = n;
+}
+if(idx == 0)
+{
+new_node->next = copy;
+*head = new_node;
+return(new_node);
+}
+for (node = 0; node < (idx - 1); node++)
+{
+if (copy == NULL || copy->next == NULL)
+return(NULL);
+copy = copy->next;
+}
+new_node->next = copy->next;
+copy->next = new_node;
+return(new_node);
+}
