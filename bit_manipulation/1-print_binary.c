c7053f38640264b2e0ce2f157bb28b258f97c36a
imenmansouri93 <imennmansourii@gmail.com>
2022-07-19 00:24:38 +0100

    printbinary

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@ void print_binary(unsigned long int n)
     int i;
     unsigned long int k;
     int print = 0;
-    for (i = 32; i >= 0; i--)
+    for (i = 65; i >= 0; i--)
     {
         k = n >> i;
         if (k & 1)
