ebefea59a0d0a07ae83c190505a11a440cfa8c68
imenmansouri93 <imennmansourii@gmail.com>
2022-07-19 18:45:06 +0100

    read-textfile

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,22 @@
+#include "main.h"
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *buffer = NULL;
+unsigned int read_size;
+FILE *handler = fopen(filename, "r");
+if (handler)
+{
+fseek(handler, 0, SEEK_END);
+letters = ftell(handler);
+rewind(handler);
+buffer = (char *)malloc(sizeof(char) * (letters + 1));
+read_size = fread(buffer, sizeof(char), letters, handler);
+buffer[letters] = '\0';
+if (letters != read_size)
+{
+    free(buffer);
+    buffer = NULL;
+}
+}
+return(0);
+}
