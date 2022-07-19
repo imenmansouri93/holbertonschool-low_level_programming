b851a7c4f8c1e62a974c272df429dea0e38a258e
imenmansouri93 <imennmansourii@gmail.com>
2022-07-19 19:05:20 +0100

    read-textfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,19 +1,20 @@
 #include "main.h"
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buffer = NULL;
+char *buffer;
 unsigned int read_size;
 FILE *handler = fopen(filename, "r");
-if (filename == NULL)
-{
-return(0);
-}
 if (handler)
 {
 fseek(handler, 0, SEEK_END);
 letters = ftell(handler);
 rewind(handler);
 buffer = (char *)malloc(sizeof(char) * (letters + 1));
+if (buffer == NULL)
+{
+return(0);
+}
+
 read_size = fread(buffer, sizeof(char), letters, handler);
 buffer[letters] = '\0';
 if (letters != read_size)
