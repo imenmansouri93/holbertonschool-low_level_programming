9469b410a7832e15c3a8bdf35ff29c738c017d70
imenmansouri93 <imennmansourii@gmail.com>
2022-07-21 00:13:16 +0100

    print all element

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * @brief 
+ * 
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+const dlistint_t *current;
+unsigned int i;
+current = h;
+if (h == NULL)
+{
+printf("list is empty\n");
+return(0);
+}
+for ( i = 0; current !=  NULL; i++)
+{
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+}
+while (current != NULL)
+{
+printf("%d", current->n);
+current = current->prev;
+}
+}
+return(i);
+}
