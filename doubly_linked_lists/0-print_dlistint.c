e31c76ef1e1d5c4b09e1309cde6c7db96c4932f6
imenmansouri93 <imennmansourii@gmail.com>
2022-07-21 00:35:18 +0100

    print all element

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -6,24 +6,20 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 const dlistint_t *current;
-unsigned int i = 0;
 current = h;
 if (h == NULL)
 {
 return(0);
 }
-for ( i = 0; current !=  NULL; i++)
-{
-while (current != NULL)
-{
-printf("%d\n", current->n);
-current = current->next;
-}
-while (current != NULL)
-{
-printf("%d", current->n);
-current = current->prev;
-}
-}
-return(i);
+
+while (current->next != NULL) {
+    current = current->next; 
+  }
+
+  while (current != NULL) {
+    printf("%d\n", current->n);
+    current = current->prev;
+  }
+
+return(1);
 }
