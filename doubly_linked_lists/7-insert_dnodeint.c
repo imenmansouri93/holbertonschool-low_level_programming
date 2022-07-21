771fd792fdeb4026c300f1e7c4db3c9ecfb8e6fa
imenmansouri93 <imennmansourii@gmail.com>
2022-07-21 16:51:26 +0100

    function that inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ if (new_node == NULL)
 return (NULL);
 }
 new_node->n = n;
-if ((h == NULL)|| (copy->next = NULL && copy == NULL && idx !=  0))
+if ((h == NULL)|| (copy->next = NULL && idx !=  0))
 return (NULL);
 if (idx == 0){
 return(add_dnodeint(h, n));
