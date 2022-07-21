6de018536967c43d8982c0f20c188c83f7046898
imenmansouri93 <imennmansourii@gmail.com>
2022-07-21 17:25:09 +0100

    readtext

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int lprint, lread, closed, file;
+size_t lprint, lread, closed, file;
 char *buffer;
 file = lprint = lread = 0;
 buffer = malloc (letters * sizeof(char));
@@ -28,7 +28,7 @@ closed = close(file);
 if (closed == -1)
 {
 return(0);
-}free(buffer);
+}
+free(buffer);
 return(lread);
-
 }
