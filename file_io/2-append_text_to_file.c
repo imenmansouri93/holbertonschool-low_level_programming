6ead1f404bff420f35a88ee2deb6e1b76da1dfa1
imenmansouri93 <imennmansourii@gmail.com>
2022-07-21 22:47:00 +0100

    append_text

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,20 +10,29 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 int fd, fdwt;
 int len = 0;
-len = strlen(text_content);
+len = _strlen(text_content);
 if (filename == NULL)
 {
 return (-1);
 }
 fd = open(filename, O_RDONLY | O_APPEND);
 fdwt = write(fd, text_content, len);
-if (text_content != NULL)
+if (text_content)
 {
-if(fdwt == -1)
+if (fdwt == -1)
 return (-1);
 }
 if (fd == -1)
 return (-1);
 close(fd);
 return (1);
+}
+int _strlen(char *str)
+{
+int l = 0;
+while (str[l])
+{
+l++;
+}
+return(l);
 }
