e5547f41cc9bbc4f3d4c8ba48bf8ab2096d5f85c
imenmansouri93 <imennmansourii@gmail.com>
2022-07-21 22:29:33 +0100

    read and printfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,12 +2,12 @@
 /**
  * read_textfile - that reads a text file and prints it to the POSIX stdout.
  * @filename: name of the file
- * @letters: ...
+ * @letters: the  numbers of letters should read and print
  * Return: letters it couls read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t lprint, lread, file, closed;
+ssize_t lprint, lread, file;
 char *buffer;
 if (filename == NULL)
 {
@@ -20,14 +20,13 @@ return (0);
 }
 file  = open(filename, O_RDONLY);
 lread = read(file, buffer, letters);
-lprint = write(STDIN_FILENO, buffer, lread);
-closed = close(file);
+lprint = write(STDOUT_FILENO, buffer, lread);
 if (file == -1 || lprint ==  -1 || lread == -1 || lprint != lread)
 {
 free(buffer);
 return (0);
 }
 free(buffer);
-closed;
+close((file));
 return (lprint);
 }
