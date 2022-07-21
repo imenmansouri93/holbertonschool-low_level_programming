a9248cec70b657ea69e793e5ef57c891e48e27b3
imenmansouri93 <imennmansourii@gmail.com>
2022-07-21 14:59:38 +0100

    free dlistint

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * @brief 
+ * 
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *current = head;
+    while (current != NULL)
+    {
+    dlistint_t* next =current->next;
+    free(current);
+    current = next;
+    }
+}
