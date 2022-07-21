35fdbaeb7eaa5e097bd29e630820903b1e7fb730
imenmansouri93 <imennmansourii@gmail.com>
2022-07-21 22:18:47 +0100

    read and printfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,26 +1,32 @@
 #include "main.h"
+/**
+ * read_textfile - that reads a text file and prints it to the POSIX stdout.
+ * @filename: name of the file
+ * @letters: ...
+ * Return: letters it couls read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 ssize_t lprint, lread, file;
 char *buffer;
 if (filename == NULL)
 {
-return(0);
+return (0);
 }
-buffer = malloc (sizeof(char)*letters);
+buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
 {
-return(0);
+return (0);
 }
 file  = open(filename, O_RDONLY);
 lread = read(file, buffer, letters);
 lprint = write(STDIN_FILENO, buffer, lread);
-if (file == -1 || lprint ==  -1 || lread == -1)
+if (file == -1 || lprint ==  -1 || lread == -1 || lprint != lread)
 {
- free(buffer);
+free(buffer);
 return (0);
 }
 free(buffer);
 close(file);
-return(lprint);
+return (lprint);
 }
