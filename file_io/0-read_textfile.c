008e12027fa8e1c6b31691d988cd095ac1407ef3
imenmansouri93 <imennmansourii@gmail.com>
2022-07-21 21:45:36 +0100

    readfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,32 +3,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 size_t lprint, lread, closed, file;
 char *buffer;
-file = lprint = lread = 0;
-buffer = malloc (letters * sizeof(char));
 if (filename == NULL)
 {
 return(0);
 }
-file  = open(filename, O_RDONLY);
-if (file == -1)
+buffer = malloc (sizeof(char)*letters);
+if (buffer == NULL)
 {
+return(0);
+}
+file  = open(filename, O_RDONLY);
+lread = read(file, buffer, letters);
+lprint = write(STDIN_FILENO, buffer, lread);
+closed = close(file);
+if (file == -1 || lprint ==  -1 || lread == -1)
+{   free(buffer);
 return (0);
 }
-lread = read(file, buffer, letters);
-if (lread == -1)
-{
-return(0);
-}
-lprint = write(STDIN_FILENO, buffer, lread);
-if (lprint ==  -1)
-{
-return(0);
-}
-closed = close(file);
 if (closed == -1)
 {
 return(0);
 }
 free(buffer);
-return(lread);
+return(lprint);
 }
