13cfd89622ed950865ea3c498169b94cc0f18664
imenmansouri93 <imennmansourii@gmail.com>
2022-07-21 14:36:46 +0100

    headerfile

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,6 +8,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 dlistint_t *newNode;
 dlistint_t *last = *head;
 newNode = malloc(sizeof(dlistint_t));
+if (newNode == NULL)
+{
+return(NULL);
+}
 newNode->n = n;
 newNode->next = NULL;
 if (*head == NULL)
