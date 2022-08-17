0f24c3aaa8c5a138722aa8496356822f933b5823
imenmansouri93 <imennmansourii@gmail.com>
2022-08-17 23:13:24 +0100

    create a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -18,6 +18,5 @@ return (NULL);
 table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 for (i = 0; i < table->size; i++)
 table->array[i] =  NULL;
-free (table);
 return (table);
 }
