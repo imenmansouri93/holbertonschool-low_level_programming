f300966114eda94cea2e8888dba68cde005dcd88
imenmansouri93 <imennmansourii@gmail.com>
2022-08-18 23:07:46 +0100

    function that adds an element to the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,14 +1,16 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 /**
- * @brief 
- * 
+ * new_t_node - create new node
+ * @key: key
+ * @value: value associated with the key
+ * Return: newnode
  */
 hash_node_t *new_t_node(char *key, char *value)
 {
 node_pointer new_node;
 new_node = malloc(sizeof(hash_node_t));
-if(new_node == NULL)
+if (new_node == NULL)
 {
 return (NULL);
 }
@@ -17,12 +19,19 @@ new_node->value = value;
 new_node->next = 0;
 return (new_node);
 }
+/**
+ * hash_table_set -  function that adds an element to the hash table.
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with the key.
+ * Return: int
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 node_pointer  head, tmp, new_node;
 char_pointer val, ky;
 unsigned long int map;
-if (strlen(key) == 0 || !key|| !ht)
+if (strlen(key) == 0 || !key || !ht)
 {
 return (0);
 }
@@ -34,7 +43,7 @@ tmp = head;
 if (head == NULL)
 {
 new_node = new_t_node(ky, val);
-if(!new_node)
+if (!new_node)
 return (0);
 ht->array[map] = new_node;
 return (1);
@@ -49,9 +58,8 @@ return (1);
 tmp = tmp->next;
 }
 new_node = new_t_node(ky, val);
-if(!new_node)
+if (!new_node)
 return (0);
 new_node->next = head;
-
 return (1);
 }
