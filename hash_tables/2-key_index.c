577c5baf574a11f68ec52f487c279fea9dee8da6
imenmansouri93 <imennmansourii@gmail.com>
2022-08-18 19:14:23 +0100

    function that gives you the index of a key

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,11 +1,10 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 /**
- * @brief 
- * 
- * @param key 
- * @param size 
- * @return unsigned long int 
+ * key_index - function that gives you the index of a key.
+ * @key: the key
+ * @size: the size of the array
+ * Return: the index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
