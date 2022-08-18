ac2ad35ad3facdcd41dbfc4a85cd5d9cd7ce155b
imenmansouri93 <imennmansourii@gmail.com>
2022-08-18 22:50:09 +0100

    unction that adds an element to the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -4,10 +4,54 @@
  * @brief 
  * 
  */
+hash_node_t *new_t_node(char *key, char *value)
+{
+node_pointer new_node;
+new_node = malloc(sizeof(hash_node_t));
+if(new_node == NULL)
+{
+return (NULL);
+}
+new_node->key = key;
+new_node->value = value;
+new_node->next = 0;
+return (new_node);
+}
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-ht = (hash_node_t*)malloc(sizeof(hash_node_t ));
-ht-> key = (char*)malloc (strlen(key) + 1);
-ht-> value = (char*)malloc (strlen(value) + 1);
-strcpy(ht->key, key);
+node_pointer  head, tmp, new_node;
+char_pointer val, ky;
+unsigned long int map;
+if (strlen(key) == 0 || key == NULL || !ht)
+{
+return (0);
+}
+val = strdup(value);
+ky = strdup(key);
+map = key_index((unsigned char *)key, ht->size);
+head  = ht->array[map];
+tmp = head;
+if (head == NULL)
+{
+new_node = new_t_node(ky, val);
+if(new_node == NULL)
+return (0);
+ht->array[map] = new_node;
+return (1);
+}
+while (tmp)
+{
+if (strcmp(tmp->key, ky)  == 1)
+{
+tmp->value = val;
+return (1);
+}
+tmp = tmp->next;
+}
+new_node =new_t_node(ky, val);
+if(new_node == NULL)
+return (0);
+new_node->next = head;
+head = new_node;
+return (1);
 }
