487426ea0ad17ba5f9089b456bcf235052edbb26
imenmansouri93 <imennmansourii@gmail.com>
2022-08-19 22:24:36 +0100

    printtable

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * @brief 
+ * 
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+node_pointer address;
+unsigned long int index, c = 0, i = 0;
+for (index = 0; index < ht->size; index++)
+{
+address = ht->array[index];
+while (address)
+{
+c++;
+address = address->next;
+}
+}
+printf("{");
+for (index = 0; index < ht->size; index++)
+{
+address = ht->array[index];
+while (address)
+{
+printf("'%s: '%s'", address->key, address->value);
+if (i < c -1)
+printf(", ");
+i++;
+address =  address->next;
+}
+}
+printf("}\n");
+}
