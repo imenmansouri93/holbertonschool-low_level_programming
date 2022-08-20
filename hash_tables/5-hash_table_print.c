77b3f55d51a0f3bcf565e7ea82e0bba33051dbba
imenmansouri93 <imennmansourii@gmail.com>
2022-08-20 18:37:37 +0100

    print

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,29 +1,29 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 /**
- * @brief 
- * 
+ * @brief
+ *
  */
 void hash_table_print(const hash_table_t *ht)
 {
-hash_node_t *node;
-unsigned long int index;
-char  *separator;
-if (ht == NULL)
-{
-return;
-}
-printf("{");
-separator = "";
-for (index = 0; index < ht->size; index++)
-{
-node = ht->array[index];
-while (node)
-{
-printf("%s'%s': '%s'", separator, node->key, node->value);
-separator =", ";
-node = node->next;
-}
-}
-printf("}\n");
+    hash_node_t *node;
+    unsigned long int index;
+    char *separator;
+    if (ht == NULL)
+    {
+        return;
+    }
+    printf("{");
+    separator = "";
+    for (index = 0; index < ht->size; index++)
+    {
+        node = ht->array[index];
+        while (node)
+        {
+            printf("%s'%s': '%s'", separator, node->key, node->value);
+            separator = ", ";
+            node = node->next;
+        }
+    }
+    printf("}\n");
 }
