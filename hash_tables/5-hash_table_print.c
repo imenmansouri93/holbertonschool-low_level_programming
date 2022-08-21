c06a2388d4dc8f755db4207b17d5413460ec8c02
imenmansouri93 <imennmansourii@gmail.com>
2022-08-21 11:38:33 +0100

    printhashtable

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -8,21 +8,25 @@
 void hash_table_print(const hash_table_t *ht)
 {
 hash_node_t *node;
-unsigned long int index;
-char *separator;
-if (ht == NULL)
+unsigned long int index = 0;
+int flag = 0;
+if (!ht)
 {
 return;
 }
 printf("{");
-separator = "";
 for (index = 0; index < ht->size; index++)
 {
 node = ht->array[index];
 while (node != NULL)
 {
-printf("%s'%s': '%s'", separator, node->key, node->value);
-separator = ", ";
+if (flag == 0)
+{
+printf("'%s': '%s'", node->key, node->value);
+flag = 1;
+}
+else
+printf(", '%s': '%s'", node->key, node->value);
 node = node->next;
 }
 }
