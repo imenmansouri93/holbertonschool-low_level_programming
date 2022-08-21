2dfe2a4e79715c069c1d81810087a13b8818ae04
imenmansouri93 <imennmansourii@gmail.com>
2022-08-21 11:46:56 +0100

    deletehashtable

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,15 +1,16 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 /**
- * @brief 
- * 
+ * hash_table_delete - delete hash table
+ * @ht: hash table
+ * Return: Nothing
  */
 void hash_table_delete(hash_table_t *ht)
 {
 unsigned int index = 0;
 hash_node_t *node1 = NULL;
 hash_node_t *node2 = NULL;
-if(!ht)
+if (!ht)
 return;
 for (index = 0; index < ht->size; index++)
 {
@@ -20,7 +21,7 @@ node2 = node1->next;
 free(node1->key);
 free(node1->value);
 free(node1);
-node1  = node2;
+node1 = node2;
 }
 }
 free(ht->array);
