20e8e6341731e82b0870f8b80943f55021db2f69
imenmansouri93 <imennmansourii@gmail.com>
2022-08-28 22:05:49 +0100

    searchvalue

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,7 +1,10 @@
 #include "search_algos.h"
 /**
- * @brief
- * 
+ *  linear_search - searches for a value in a sorted array of integers
+ * @array: is a pointer to the first element of the array to search in
+ * @size:  is the number of elements in array
+ * @value: is the value to search
+ * Return: value
  */
 int linear_search(int *array, size_t size, int value)
 {
@@ -9,7 +12,7 @@ size_t i;
 for (i = 0; i < size; i++)
 {
 printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-if(array[i] == value && array != NULL)
+if (array[i] == value && array != NULL)
 return (i);
 }
 return (-1);
