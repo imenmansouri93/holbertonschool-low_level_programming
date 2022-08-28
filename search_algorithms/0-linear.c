8e1bb01dd75a2202c7e390474f91546a81ed1ad7
imenmansouri93 <imennmansourii@gmail.com>
2022-08-28 21:50:38 +0100

    11

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -7,6 +7,10 @@ int linear_search(int *array, size_t size, int value)
 {
 size_t i;
 for (i = 0; i < size; i++)
+if(array != NULL)
+{
+printf("Value checked array[%ld] = [%d]\n",i, array[i]);
+}
 if(array[i] == value && array != NULL)
 {
 return (i);
