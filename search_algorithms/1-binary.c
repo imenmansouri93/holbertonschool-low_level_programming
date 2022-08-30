f3dc7a19ffe017dce308c29abbbf17bd435a54dd
imenmansouri93 <imennmansourii@gmail.com>
2022-08-30 08:44:00 +0100

    searchbinary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,7 +1,10 @@
 #include "search_algos.h"
 /**
- * @brief
- * 
+ * print_array - print array
+ * @array: ointer to the first element of the array
+ * @low: the begin of table
+ * @high: the end of table
+ * Return: Nothing
  */
 void print_array(int *array, size_t low, size_t high)
 {
@@ -14,9 +17,15 @@ printf(", %i", array[i]);
 else
 printf("%i", array[i]);
 }
-printf ("\n");
+printf("\n");
 }
-
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * @array:  pointer to the first element of the array
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: index to the value
+ */
 int binary_search(int *array, size_t size, int value)
 {
 size_t  begin = 0, md, end = size -  1;
@@ -26,13 +35,13 @@ return (-1);
 }
 while (begin <= end)
 {
-md = (begin + end)/2;
+md = (begin + end) / 2;
 print_array(array, begin, end);
 if (array[md] < value)
 begin = md + 1;
 else if (array[md] > value)
 end = md - 1;
-else 
+else
 return (md);
 }
 return (-1);
